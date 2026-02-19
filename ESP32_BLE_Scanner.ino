035caebde45e00848459484fe316db9c976e893e
jjl034 <jjl034@latech.edu>
2026-02-19 14:38:03 -0600

    Update ESP32_BLE_Scanner.ino

--- ESP32_BLE_Scanner.ino
+++ ESP32_BLE_Scanner.ino
@@ -1,63 +1,82 @@
+#include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <BLEDevice.h>
+#include <BLEUtils.h>
 #include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <ArduinoJson.h>
 
-const char* ssid = "YOUR_WIFI";
-const char* password = "YOUR_PASS";
+// --- WiFi ---
+const char* ssid = "iPhone (4)";
+const char* password = "Fluffy11";
 
-const char* mqtt_server = "192.168.1.37";
+// --- MQTT ---
+const char* mqtt_server = "172.20.10.9";  // Raspberry Pi IP
 const int mqtt_port = 1883;
+const char* mqtt_topic = "edc/missing";
 
+// --- Device Location ---
+const char* locationName = "Kitchen"; // Change for each ESP32
+
+// --- BLE ---
+int scanTime = 5;
+BLEScan* pBLEScan;
+
+// --- MQTT Client ---
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-BLEScan* pBLEScan;
-int scanTime = 5;
-
-void setup_wifi() {
+void connectWiFi() {
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
+  while (WiFi.status() != WL_CONNECTED) delay(500);
 }
 
-void reconnect() {
+void connectMQTT() {
   while (!client.connected()) {
-    client.connect("ESP32_BedroomScanner");
+    if (!client.connect(locationName)) delay(2000);
   }
 }
 
 void setup() {
   Serial.begin(115200);
-  setup_wifi();
+  connectWiFi();
   client.setServer(mqtt_server, mqtt_port);
 
   BLEDevice::init("");
   pBLEScan = BLEDevice::getScan();
   pBLEScan->setActiveScan(true);
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);
 }
 
 void loop() {
-  if (!client.connected()) reconnect();
+  if (!client.connected()) connectMQTT();
   client.loop();
 
   BLEScanResults* foundDevices = pBLEScan->start(scanTime, false);
+  int count = foundDevices->getCount();
 
-  for (int i = 0; i < foundDevices->getCount(); i++) {
+  for (int i = 0; i < count; i++) {
     BLEAdvertisedDevice device = foundDevices->getDevice(i);
-
-    String mac = device.getAddress().toString().c_str();
-    mac.toLowerCase();   // normalize MAC
-
     int rssi = device.getRSSI();
 
-    String topic = "edc/items/" + mac + "/seen";
-    String payload = "{\"location\":\"bedroom\",\"rssi\":" + String(rssi) + "}";
+    if (rssi >= -60) {  // Only close devices
+      String name = device.getName().c_str();
+      String mac = device.getAddress().toString().c_str();
 
-    client.publish(topic.c_str(), payload.c_str());
+      StaticJsonDocument<256> doc;
+      doc["mac"] = mac;
+      doc["name"] = name;
+      doc["rssi"] = rssi;
+      doc["last_seen"] = locationName;
+
+      char buffer[256];
+      serializeJson(doc, buffer);
+      client.publish(mqtt_topic, buffer);
+    }
   }
 
   pBLEScan->clearResults();
-  delay(5000);
+  delay(3000);
 }
